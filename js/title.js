b9b5bb27e84df3aaa3de8b1ae96586795fe75046
Nick Bond <bond.n@husky.neu.edu>
2017-08-07 10:07:19 -0400

    responsive title

--- js/title.js
+++ js/title.js
@@ -1,10 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var nickBond = document.getElementById("nick-bond");
-  var titleBackground = document.getElementById("title-background");
+function positionTitle() {
+  if (window.innerWidth > 600) {
+    var nickBond = document.getElementById("nick-bond");
+    var titleBackground = document.getElementById("title-background");
 
-  var nickBondMargin = titleBackground.clientWidth / 2;
-  var titleBackgroundMargin = nickBond.clientWidth / 2;
+    var nickBondMargin = titleBackground.clientWidth / 2;
+    var titleBackgroundMargin = nickBond.clientWidth / 2;
 
-  nickBond.style.marginRight = "-" + titleBackgroundMargin + "px";
-  titleBackground.style.marginLeft = "-" + nickBondMargin + "px";
-});
+    nickBond.style.marginRight = "-" + titleBackgroundMargin + "px";
+    titleBackground.style.marginLeft = "-" + nickBondMargin + "px";
+  } else {
+    var nickBond = document.getElementById("nick-bond");
+    nickBond.style.marginRight = "0";
+  }
+}
+
+window.addEventListener("resize", positionTitle);
+document.addEventListener("DOMContentLoaded", positionTitle);
